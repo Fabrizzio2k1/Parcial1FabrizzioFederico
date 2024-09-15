96c52b42734543e5bdb3f8f49dae6d3d89d9c548
Fabrizzio2k1 <fabrizzio.federico@davinci.edu.ar>
2024-09-15 14:53:27 -0300

    Etapa 1

--- src/EjercicioBiblioteca/Libro.java
+++ src/EjercicioBiblioteca/Libro.java
@@ -1,5 +1,95 @@
 package EjercicioBiblioteca;
 
-public class Libro {
+import java.time.LocalDate;
 
+public class Libro {
+	
+	//Atributos
+	private String nombre;
+	private String autor;
+	private Boolean disponibilidad;
+	private LocalDate fechaInicio;
+	private LocalDate fechaDevolucion;
+	private String persona;
+	private String dni;
+	
+	//Constructor
+	public Libro() {
+		this.nombre = "No asignado";
+		this.autor = "No asignado";
+		this.disponibilidad = null;
+		this.fechaInicio = LocalDate.now();
+		this.fechaDevolucion = LocalDate.now();
+		this.persona = "No asignado";
+		this.dni = "No asignado";
+	}
+	
+	//Getters y Setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public Boolean getDisponibilidad() {
+		return disponibilidad;
+	}
+
+	public void setDisponibilidad(Boolean disponibilidad) {
+		this.disponibilidad = disponibilidad;
+	}
+
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(LocalDate fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public LocalDate getFechaDevolucion() {
+		return fechaDevolucion;
+	}
+
+	public void setFechaDevolucion(LocalDate fechaDevolucion) {
+		this.fechaDevolucion = fechaDevolucion;
+	}
+
+	public String getPersona() {
+		return persona;
+	}
+
+	public void setPersona(String persona) {
+		this.persona = persona;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	public void solicitarLibro() {
+		
+	}
+	
+	public String validarCaracteres(String mensaje) {
+		
+		return "";
+	}
+	
+	
+	
 }
--- src/EjercicioBiblioteca/Main.java
+++ src/EjercicioBiblioteca/Main.java
@@ -1,10 +1,41 @@
 package EjercicioBiblioteca;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String menu [] = {"Solicitar","Devolver","Estado","Salir"};
+		int opcion;
+		Libro l1 = new Libro();
+		
+		do {
+			opcion = JOptionPane.showOptionDialog(null, "Bienvenido a Lectores Felices!!!\nElija una opcion:", null, 0, 0, null, menu, menu[0]);
+			switch (opcion) {
+			case 0:
+				break;
+			case 1:
+				if (l1.getNombre().equals("No asignado")) {
+					JOptionPane.showMessageDialog(null, "No solicitaste ningun libro...");
+				} else {
+					// Aca va la devolucion
+				}
+				break;
+			case 2:
+				if (l1.getNombre().equals("No asignado")) {
+					JOptionPane.showMessageDialog(null, "No solicitaste ningun libro...");
+				} else {
+					// Aca va el estado
+				}
+				break;
+			case 3:
+				JOptionPane.showMessageDialog(null, "Hasta luego, que tenga un Buen dia!!!");
+				break;
+			default:
+				break;
+			}
+		} while (opcion!=3);
 	}
 
 }

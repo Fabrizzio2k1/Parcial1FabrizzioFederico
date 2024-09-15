afbdd1fbe3f56af126b4378ebca4e8c057d64e1f
Fabrizzio2k1 <fabrizzio.federico@davinci.edu.ar>
2024-09-15 20:01:04 -0300

    Contructores gettes y setters

--- src/EjercicioBairesRent/BairesRent.java
+++ src/EjercicioBairesRent/BairesRent.java
@@ -1,5 +1,90 @@
 package EjercicioBairesRent;
 
-public class BairesRent {
+import java.time.LocalDate;
 
+public class BairesRent {
+	//Atributos
+	
+	private int precio;
+	private Boolean disponibilidad;
+	private double tamaño;
+	private String ubicacion;
+	private LocalDate fecha;
+	private String inquilino;
+	private int dni;
+	
+	//Constructor
+	public BairesRent() {
+		this.precio = 0;
+		this.disponibilidad = true;
+		this.tamaño = 0.0;
+		this.ubicacion = "No asignado";
+		this.fecha = LocalDate.now();
+		this.inquilino = "No asignado";
+		this.dni = 0;
+	}
+	
+	//Getters y Setters
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public Boolean getDisponibilidad() {
+		return disponibilidad;
+	}
+
+	public void setDisponibilidad(Boolean disponibilidad) {
+		this.disponibilidad = disponibilidad;
+	}
+
+	public double getTamaño() {
+		return tamaño;
+	}
+
+	public void setTamaño(double tamaño) {
+		this.tamaño = tamaño;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getInquilino() {
+		return inquilino;
+	}
+
+	public void setInquilino(String inquilino) {
+		this.inquilino = inquilino;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	
+	public void ingresarCuenta() {
+		
+	}
+	
+	
 }
--- src/EjercicioBairesRent/Main.java
+++ src/EjercicioBairesRent/Main.java
@@ -1,10 +1,25 @@
 package EjercicioBairesRent;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String menu [] = {"Ingreso","Consulta","Alquilar","Salir"};
+		int opcion;
+		
+		do {
+			opcion = JOptionPane.showOptionDialog(null, "Seleccione una opción", null, 0, 0, null, menu, menu[0]);
+			switch (opcion) {
+			case 0:
+				
+				break;
 
+			default:
+				break;
+			}
+		} while (opcion!=3);
 	}
 
 }
